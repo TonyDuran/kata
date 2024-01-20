4d8b0f4d50e1819460ce5c38aae7efff5adb8898
Tony Duran <tony-duran@outlook.com>
2024-01-20 14:52:34 -0600

    figured out the optimal solution for 167

--- LeetCode/Python/167_two_sum_part_2.py
+++ LeetCode/Python/167_two_sum_part_2.py
@@ -25,22 +25,20 @@ def test_scenarios(test_input, expected):
     assert Solution().twoSum(*test_input) == expected
 
 
+# This is a faster solution. I think O(log n)
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l_ptr = 0
         r_ptr = len(numbers) - 1
-        while r_ptr > 0:
-            while l_ptr < r_ptr:
-                if (numbers[l_ptr] + numbers[r_ptr]) == target:
-                    return [l_ptr + 1, r_ptr + 1]
-                # NOTE: this speeds things up, but still slow solution
-                elif (numbers[l_ptr] + numbers[r_ptr]) > target :
-                    break
-                else:
-                    l_ptr += 1
-            r_ptr -= 1
-            l_ptr = 0
-        return[0, 0]
+        while l_ptr != r_ptr:
+            if (numbers[l_ptr] + numbers[r_ptr]) > target:
+                r_ptr -= 1
+            elif (numbers[l_ptr] + numbers[r_ptr]) < target:
+                l_ptr += 1
+            else:
+                return [l_ptr + 1, r_ptr + 1]
+        return [0, 0]
+
 
 
 
